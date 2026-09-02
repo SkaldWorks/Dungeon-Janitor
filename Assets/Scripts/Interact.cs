f406fa6435c6790ff923f9d6101408436eb12710
Zek <melech409@gmail.com>
2026-09-02 09:50:20 -0500

    fix package manager issues and made basic scene

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,58 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public interface IInteractable
+{
+    void Interact();
+    void OnTouchingPlayer();
+    void OnNotTouchingPlayer();
+
+}
+public class Interact : MonoBehaviour
+{
+    private IInteractable currentInteractable;
+    public GameObject interactionUI;
+    public TextMeshProUGUI interactionText;
+
+    void Start()
+    {
+        interactionUI.SetActive(false);
+    }
+    void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+            interactionUI.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        IInteractable interactable = collision.GetComponent<IInteractable>();
+
+        if (interactable != null)
+        {
+            interactionUI.SetActive(true);
+            currentInteractable = interactable;
+            currentInteractable.OnTouchingPlayer();
+
+
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        IInteractable interactable = collision.GetComponent<IInteractable>();
+
+        if (interactable != null && interactable == currentInteractable)
+        {
+            interactionUI.SetActive(false);
+
+            currentInteractable.OnNotTouchingPlayer();
+            currentInteractable = null;
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float sprintSpeed;
+    public float groundDist;
+
+    public LayerMask ground;
+    public Rigidbody rb;
+    public SpriteRenderer sr;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, ground))
+        {
+            if (hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDist;
+                transform.position = movePos;
+            }
+        }
+
+        float x = 0;
+        float y = 0;
+
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed)
+                x = -1;
+            if (Keyboard.current.dKey.isPressed)
+                x = 1;
+            if (Keyboard.current.sKey.isPressed)
+                y = -1;
+            if (Keyboard.current.wKey.isPressed)
+                y = 1;
+        }
+
+        Vector3 moveDir = new Vector3(x, 0, y);
+        rb.linearVelocity = moveDir * speed;
+
+        if (x != 0 && x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (x != 0 && x > 0)
+        {
+            sr.flipX = false;
+        }
+    }}
