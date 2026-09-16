cc5a97a624ca47305bd06df33447d7f81f7830ad
Zek <skaldworks409@gmail.com>
2026-09-16 16:12:58 -0500

    added temporary art

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,56 +9,84 @@ public class PlayerController : MonoBehaviour
     public LayerMask ground;
     public Rigidbody rb;
     public SpriteRenderer sr;
+
     public bool freeze = false;
+
+    private Vector2 moveInput;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
     }
 
     void Update()
     {
-        if (!freeze)
+        if (freeze)
         {
-            RaycastHit hit;
-            Vector3 castPos = transform.position;
-            castPos.y += 1;
-            if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, ground))
-            {
-                if (hit.collider != null)
-                {
-                    Vector3 movePos = transform.position;
-                    movePos.y = hit.point.y + groundDist;
-                    transform.position = movePos;
-                }
-            }
-        
+            moveInput = Vector2.zero;
+            return;
+        }
 
-            float x = 0;
-            float y = 0;
+        float x = 0;
+        float y = 0;
 
-            if (Keyboard.current != null)
-            {
-                if (Keyboard.current.aKey.isPressed)
-                    x = -1;
-                if (Keyboard.current.dKey.isPressed)
-                    x = 1;
-                if (Keyboard.current.sKey.isPressed)
-                    y = -1;
-                if (Keyboard.current.wKey.isPressed)
-                    y = 1;
-            }
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed)
+                x = -1;
 
-            Vector3 moveDir = new Vector3(x, 0, y);
-            rb.linearVelocity = moveDir * speed;
+            if (Keyboard.current.dKey.isPressed)
+                x = 1;
 
-            if (x != 0 && x < 0)
-            {
-                sr.flipX = true;
-            }
-            else if (x != 0 && x > 0)
-            {
-                sr.flipX = false;
-            }
+            if (Keyboard.current.sKey.isPressed)
+                y = -1;
+
+            if (Keyboard.current.wKey.isPressed)
+                y = 1;
+        }
+
+        moveInput = new Vector2(x, y);
+
+        // Sprite flipping doesn't need to happen in FixedUpdate
+        if (x < 0)
+            sr.flipX = true;
+        else if (x > 0)
+            sr.flipX = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (freeze)
+        {
+            rb.linearVelocity = Vector3.zero;
+            return;
+        }
+
+        Vector3 moveDir = new Vector3(
+            moveInput.x,
+            0,
+            moveInput.y
+        );
+
+        rb.linearVelocity = moveDir * speed;
+
+        RaycastHit hit;
+
+        Vector3 castPos = rb.position;
+        castPos.y += 1;
+
+        if (Physics.Raycast(
+            castPos,
+            Vector3.down,
+            out hit,
+            Mathf.Infinity,
+            ground))
+        {
+            Vector3 movePos = rb.position;
+            movePos.y = hit.point.y + groundDist;
+
+            rb.MovePosition(movePos);
         }
     }
 }
